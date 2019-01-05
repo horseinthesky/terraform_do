3a205e258cdeb50f66dcb3f9ebb350c6a9f9e782
horseinthesky <pwnedbyspawn@gmail.com>
2019-01-05 22:44:04 +0300

    Deleted ansible hosts template

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
-data "template_file" "ansible_hosts" {
-  template = "${file("${path.module}/config_templates/hosts.tpl")}"
-  vars {
-    public_ip = "${digitalocean_droplet.vps.ipv4_address}"
-  }
-}
-
-output "Public ip" {
-  value = "${data.template_file.ansible_hosts.rendered}"
+output "VPS public ip" {
+  value = "${digitalocean_droplet.vps.ipv4_address}"
 }
